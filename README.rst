5eb5f39e273e7a8e69d9d1606d6f22f0373cddd4
Erik Johansson <erik@ejohansson.se>
2014-01-20 21:39:39 +0100

    Add some simple setup.py and README.rst files

---
+++ README.rst
@@ -0,0 +1,17 @@
+Python wrapper for Telldus Live
+===============================
+
+tellive-py is a Python wrapper for `Telldus Live <http://live.telldus.com/>`_,
+"a user friendly service for automating your TellStick connected gear using the
+Internet".
+
+Please report any problem as a `GitHub issue report
+<https://github.com/erijo/tellive-py/issues/new>`_.
+
+Currently in very early development.
+
+Requirements
+------------
+
+* Python 3.2+
+* `tellcore-py <https://github.com/erijo/tellcore-py>`_
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tellive-py',
+    version='0.1.0',
+    author='Erik Johansson',
+    author_email='erik@ejohansson.se',
+    packages=['tellive'],
+    url='https://github.com/erijo/tellive-py',
+    license='GPLv3+',
+    description='Python wrapper for connecting to Telldus Live',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        ],
+)
