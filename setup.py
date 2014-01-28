94bc1f8444a562182a4890f5aed6fd2df20e06df
Erik Johansson <erik@ejohansson.se>
2014-01-28 22:14:08 +0100

    build: read version from __init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import re
+
+version_re = re.compile(r'__version__ = "(.*)"')
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(cwd, 'tellive', '__init__.py')) as init:
+    for line in init:
+        match = version_re.search(line)
+        if match:
+            version = match.group(1)
+            break
+    else:
+        raise Exception('Cannot find version in __init__.py')
 
 setup(
     name='tellive-py',
-    version='0.1.0',
+    version=version,
     author='Erik Johansson',
     author_email='erik@ejohansson.se',
     packages=['tellive'],
@@ -14,13 +28,18 @@ setup(
     url='https://github.com/erijo/tellive-py',
     license='GPLv3+',
     description='Python wrapper for connecting to Telldus Live',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst').read() + '\n\n' + \
+        open('CHANGES.rst').read(),
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 )
